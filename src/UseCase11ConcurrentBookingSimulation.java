3ab953b1dc2ba9ed342e95d14e09e8cdbdc5ab3a
Sindhuja-R-23 <sk9028@srmist.edu>
2026-03-30 20:53:14 +0530

    UC11: Multithreading

---
+++ src/UseCase11ConcurrentBookingSimulation.java
@@ -0,0 +1,8 @@
+public class UseCase11ConcurrentBookingSimulation {
+    public static void main(String[] args) {
+        Runnable r = () -> System.out.println(Thread.currentThread().getName());
+
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+}
