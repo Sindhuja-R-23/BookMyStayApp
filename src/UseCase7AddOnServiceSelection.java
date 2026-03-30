cf84e48aad3929bc746cc2cb81b2eb37dbeaac09
Sindhuja-R-23 <sk9028@srmist.edu>
2026-03-30 20:49:57 +0530

    UC7: Add-on services

---
+++ src/UseCase7AddOnServiceSelection.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class UseCase7AddOnServiceSelection {
+    public static void main(String[] args) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        map.put("R1", Arrays.asList("WiFi", "Food"));
+
+        System.out.println(map);
+    }
+}
