838c9a4fa3630baa3b0bf81170cdda758d4c0107
Sindhuja-R-23 <sk9028@srmist.edu>
2026-03-30 20:50:49 +0530

    UC8: Booking history

---
+++ src/UseCase8BookingHistoryReport.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class UseCase8BookingHistoryReport {
+    public static void main(String[] args) {
+        List<String> history = new ArrayList<>();
+        history.add("R1");
+        history.add("R2");
+
+        System.out.println(history);
+    }
+}
